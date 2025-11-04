e1cabae5dca1e504c2b14ab483cdb729ebb8e4ba
reomoon <thmoon0922@gmail.com>
2025-11-04 09:51:54 +0900

    git 캐시 제거

--- tests/front/search/test_image_search.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from tests.front.login.test_front_login_fixture import front_login_fixture
-from pages.front.search.fr_Image_Search import Image_search
-
-def test_fr_ImageSearch(front_login_fixture):
-    page = front_login_fixture    # 로그인된 페이지 사용
-    Image_search(page)    # Image Serach 실행
-    # 브라우저 닫기 (pytest fixture에서 자동으로 닫기 처리) 
